c94c786615ebe28a599f6361218ee93fc22e14dd
bjmontillon <pangilinanbj@gmail.com>
2021-08-08 23:48:17 +0800

    trial share button

--- src/Card/Cardcomponent.js
+++ src/Card/Cardcomponent.js
@@ -72,6 +72,9 @@ const Cardcomponent = (props) => {
                 <IconButton aria-label="share">
                     <ShareIcon />
                 </IconButton>
+                <IconButton aria-label="share">
+                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><ShareIcon /></a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </IconButton>
             </Card>
         </Grid>
     )
