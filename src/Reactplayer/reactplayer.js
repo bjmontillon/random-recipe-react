9c928a3260c31f5ae8baff192058fc43072ad62c
bjmontillon <pangilinanbj@gmail.com>
2021-08-12 22:31:38 +0800

    resized padding reactplayercomponent

--- src/Reactplayer/reactplayer.js
+++ src/Reactplayer/reactplayer.js
@@ -5,11 +5,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     playerContainer: {
-        padding : '10px',
+        padding : '1.5rem 0',
         height: '42vh',
         justifyContent: 'center',
         alignItems: 'center',
-        boxShadow: 'rgb(213, 210, 208) 5px 5px 15px, rgb(255, 255, 255) -5px -5px 15px',
+        
     },
     reactPLayer: {
         
